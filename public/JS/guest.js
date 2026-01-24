9c24f9fd36e18c2867b243177a225af2091c48f1
GpapPeaky <giorgospapamat@gmail.com>
2026-01-24 16:49:27 +0200

    Added Review page per band when whatever the fuck

---
+++ public/JS/guest.js
@@ -0,0 +1,34 @@
+// Guest frontend stuff.
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchBands();
+});
+
+// Guest band fetch, and html rendering
+async function fetchBands() {
+  const res = await fetch("/general/getBands");
+  const data = await res.json();
+  const bands = data.bands;
+
+  const list = document.getElementById("band_list");
+  list.innerHTML = "";
+
+  bands.forEach(b => {
+    const li = document.createElement("div");
+    li.classList.add("band-card");
+    
+    li.innerHTML = `<h3>${b.band_name}</h3>
+        <p><strong>Genre:</strong> <i> ${b.music_genres}</i></p>
+        <p>${b.band_description}</p>
+        <p><em>${b.band_city}</em></p>
+    `;
+
+    // Add band name and band_id to the query
+    li.addEventListener("click", () => {
+        window.location.href = `/reviews.html?band=${encodeURIComponent(b.band_name)}`;
+    });
+
+
+    list.appendChild(li);
+  });
+}
