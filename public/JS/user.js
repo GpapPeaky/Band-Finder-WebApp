b7ad96f1bc25b12b6e008ee2b09c186e1dd60881
GpapPeaky <giorgospapamat@gmail.com>
2026-01-25 15:35:29 +0200

    Added logged in user sends reviews or whtv tf

--- public/JS/user.js
+++ public/JS/user.js
@@ -9,3 +9,34 @@ function isLoggedIn() {
     userType !== null
   );
 }
+
+async function sendReview(bandName) {
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+
+  if (!user) return;
+
+  const username = user.username;
+  const password = user.password;
+  const rating = document.getElementById("review_rating").value;
+  const review = document.getElementById("review_text").value;
+
+  if (!rating || !review) return;
+
+  const res = await fetch("user/review", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      band_name: bandName,
+      sender: username,
+      password,
+      review,
+      rating
+    })
+  });
+
+  if (!res.ok) return;
+
+  document.getElementById("review_form").reset();
+}
